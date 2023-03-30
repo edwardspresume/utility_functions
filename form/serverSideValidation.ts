a202b05f2822cc17b0e656e3a3f2a29c7f288573
Edwards Presume <edwardspresume@gmail.com>
2023-03-30 18:07:43 -0400

    first commit

---
+++ form/serverSideValidation.ts
@@ -0,0 +1,57 @@
+import { FormField } from './types';
+
+export default function validateForm(requiredFields: FormField[]) {
+    return requiredFields.flatMap(validateInput);
+}
+
+// Define patterns for input types that require pattern validation
+const PATTERNS = {
+    email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    url: /^(http|https):\/\/[^ "]+$/,
+    phone: /^\d{3}-\d{3}-\d{4}$/,
+};
+
+// Validation rules for input fields
+function validateInput(input: FormField) {
+    const validationRules = [
+        {
+            // Check if a required input field is empty
+            test: () => !input.value.trim(),
+            message: () =>
+                `${input.name} is a required field, please enter a value`,
+        },
+        {
+            // Check if the input value length is within the specified range
+            test: () => hasInvalidLength(input),
+            message: () =>
+                `${input.name} must have at least ${input.minLength} and no more than ${input.maxLength} characters`,
+        },
+        {
+            // Check if the input value matches the specified pattern for a given input type
+            test: () => !matchesPattern(input),
+            message: () =>
+                `Please enter a valid ${input.type} for the field: ${input.name}`,
+        },
+    ];
+
+    return validationRules
+        .filter((rule) => rule.test())
+        .map((rule) => rule.message());
+}
+
+function hasInvalidLength(input: FormField) {
+    const { value, minLength, maxLength } = input;
+
+    const tooShort = minLength > 0 && value.length < minLength;
+    const tooLong = maxLength > 0 && value.length > maxLength;
+
+    return tooShort || tooLong;
+}
+
+function matchesPattern(input: FormField) {
+    const pattern = PATTERNS[input.type];
+
+    if (!pattern) return true;
+
+    return pattern.test(input.value);
+}
---
+++ form/types.ts
@@ -0,0 +1,7 @@
+export interface FormField {
+    type: string;
+    name: string;
+    value: string;
+    minLength: number;
+    maxLength: number;
+}
